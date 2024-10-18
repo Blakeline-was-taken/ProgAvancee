ee5b7d4aaa7c72496a56cefa84783805a7dd33da
loana <loan.quillet@gmail.com>
2024-10-18 08:28:14 +0200

    TP2 init

---
+++ src/tp1/TpMobile.java
@@ -0,0 +1,7 @@
+package tp1;//programme d'un mobile se deplacant de la gauche vers la droite de la fenetre-ecran
+
+public class TpMobile{
+    public static void main(String[] telsArgs){
+	    new UneFenetre();
+    }
+}
